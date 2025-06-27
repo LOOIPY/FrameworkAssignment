242c40a27732d7b73917e587da93e49cc52eba5c
Yuvarani1305 <yuvarani1305@gmail.com>
2025-06-27 18:47:54 +0800

    fixing for otp error

--- propertymanagement/settings.py
+++ propertymanagement/settings.py
@@ -170,12 +170,3 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# Email configuration (for Gmail)
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = '24073175@siswa.um.edu.my'
-EMAIL_HOST_PASSWORD = 'nyaq yhnu dwlp cmxp'
-DEFAULT_FROM_EMAIL = '24073175@siswa.um.edu.my'
